25ce4b4a708c91e8509366d9f9b7a705468930eb
Andrew Masek <andrewmasek@gmail.com>
2024-05-30 14:41:52 -0700

    Added logging for errors

--- final_particle_transformer/train.py
+++ final_particle_transformer/train.py
@@ -35,38 +35,44 @@ validation_file_dict = {'validation': glob('/home/particle/particle_transformer/
 #file_dict = {'validation':glob(r'C:\Users\andre\Desktop\UCSD\CSE145\cse145-particle-transformer\final_particle_transformer\dataloading\JetClass_Pythia_val_5M\val_5M\*.root')}
 #file_dict = {'validation': glob('C:/Users/Abiji/Documents/ABClasses/CSE145/cse145-particle-transformer/final_particle_transformer/dataloading/JetClass_Pythia_val_5M/val_5M/*.root')}
 data_config_file = './dataloading/dataconfig.yaml'
-train_dataloader = create_tf_dataloader(train_file_dict, data_config_file)
-validation_dataloader = create_tf_dataloader(validation_file_dict, data_config_file)
 
-for test in train_dataloader:
-    print("THIS IS THE DATALOADER!!!")
-    print(len(test)) # 2
-    print("something1", test[0].shape, test[1].shape) # (2, 128), ()
-    break
+try:
+    train_dataloader = create_tf_dataloader(train_file_dict, data_config_file)
+    validation_dataloader = create_tf_dataloader(validation_file_dict, data_config_file)
 
-train_dataset = train_dataloader.batch(batch_size)
-validation_dataset = validation_dataloader.batch(batch_size)
+    for test in train_dataloader:
+        print("THIS IS THE DATALOADER!!!")
+        print(len(test)) # 2
+        print("something1", test[0].shape, test[1].shape) # (2, 128), ()
+        break
 
-#for test in train_dataset:  
-#    print("THIS IS THE TRAIN DATASET!!!")
-#    print(len(test)) # 2
-#    print("something2", test[0].shape, test[1].shape) # (batch_size, 2, 128), (batch_size, )
-#    break
+    train_dataset = train_dataloader.batch(batch_size)
+    validation_dataset = validation_dataloader.batch(batch_size)
 
-model = ParticleTransformer((128, 2), num_classes=10)
-# model = ParticleTransformerTagger((1, 2, 128), )
-# model = Embed([2, 128], [128, 512, 128], activation='gelu')
+    #for test in train_dataset:  
+    #    print("THIS IS THE TRAIN DATASET!!!")
+    #    print(len(test)) # 2
+    #    print("something2", test[0].shape, test[1].shape) # (batch_size, 2, 128), (batch_size, )
+    #    break
 
-with tf.device(device):
-    print("Using ", device)
-    loss_function = k.losses.SparseCategoricalCrossentropy(from_logits=True)
-    model.compile(
-        optimizer=k.optimizers.Adam(learning_rate=learning_rate),
-        loss=loss_function,
-        metrics=['val_loss', 'val_accuracy']
-    )
-    #validation_split=validation_split, validation_batch_size=batch_size, validation_steps=20
-    #TODO validation data shouldn't be the same as training data
-    
-    model.fit(train_dataset, epochs=epochs, batch_size=batch_size, steps_per_epoch=steps_per_epoch, validation_data=validation_dataset)
-    # model.fit(train_dataset, epochs=epochs, batch_size=batch_size, steps_per_epoch=steps_per_epoch, validation_data=train_dataset)
+    model = ParticleTransformer((128, 2), num_classes=10)
+    # model = ParticleTransformerTagger((1, 2, 128), )
+    # model = Embed([2, 128], [128, 512, 128], activation='gelu')
+
+    with tf.device(device):
+        print("Using ", device)
+        loss_function = k.losses.SparseCategoricalCrossentropy(from_logits=True)
+        model.compile(
+            optimizer=k.optimizers.Adam(learning_rate=learning_rate),
+            loss=loss_function,
+            metrics=['val_loss', 'val_accuracy']
+        )
+        #validation_split=validation_split, validation_batch_size=batch_size, validation_steps=20
+        #TODO validation data shouldn't be the same as training data
+        
+        model.fit(train_dataset, epochs=epochs, batch_size=batch_size, steps_per_epoch=steps_per_epoch, validation_data=validation_dataset)
+        # model.fit(train_dataset, epochs=epochs, batch_size=batch_size, steps_per_epoch=steps_per_epoch, validation_data=train_dataset)
+except Exception as e:
+    log_file = open('./error.log', 'w')
+    log_file.write(str(e))
+    log_file.close()
