Progress towards fixing gradient missing issue

final_particle_transformer/multi_head_attention.py
@@ -19,7 +19,7 @@ class Head(keras.Model):
         
         self.dropout = keras.layers.Dropout(dropout)
 
-    def call(self, q, k):
+    def call(self, q, k, v, padding_mask=None, attn_mask=None):
         """
         Inputs: 
             q: (T, B, C)
@@ -28,7 +28,10 @@ class Head(keras.Model):
         Returns: 
             out: ...
         """
+        print('padding_mask_val', padding_mask, 'attn_mask_val', attn_mask)
         # (T, B, C) = (# of particles, batch_size, # features)
+        zero_val = tf.constant(0, dtype=tf.float32)
+        k = tf.where(padding_mask, zero_val, k)
         K = self.key(k) # (T, B, head_size)
         Q = self.query(q) # (T, B, head_size)
 
@@ -45,18 +48,21 @@ class Head(keras.Model):
 
         wei = Q @ after_transpose * q.shape[-1]**-0.5 # (B, T, T)
         # print("3: wei.shape:", wei.shape) 
-        tril = tf.convert_to_tensor(np.tril(np.ones((T, T), dtype='float_'), 0), dtype=tf.float32)
-        ninf = tf.constant(float('-inf'), dtype=tf.float32)
-        wei = tf.where(tril[:T, :T] == 0, ninf, wei) # (B, T, T)
+        #tril = tf.convert_to_tensor(np.tril(np.ones((T, T), dtype='float_'), 0), dtype=tf.float32)
+        #ninf = tf.constant(float('-inf'), dtype=tf.float32)
+        #wei = tf.where(tril[:T, :T] == 0, ninf, wei) # (B, T, T)
+        wei += attn_mask
         wei = keras.activations.softmax(wei) # (B, T, T)
         wei = self.dropout(wei)
         # perform weighted aggregation of the values
         # print("4: wei.shape:", wei.shape)
-        v = self.value(k) # (T, B, head_size)
+        #v = self.value(k) # (T, B, head_size)
         # print("5: v.shape:", v.shape)
-        V = tf.transpose(v, perm=[1, 0, 2])
+        #V = tf.transpose(v, perm=[1, 0, 2])
         # print("6: v.transpose.shape:", V.shape)
-        out = wei @ V # (B, T, T) @ (B, T, C) -> (B, T, C)
+        #out = wei @ V # (B, T, T) @ (B, T, C) -> (B, T, C)
+        V = self.value(v)
+        out = wei @ V
         # print("7: out.shape:", out.shape)
         
         # make batch dim the second dim again...
@@ -74,8 +80,8 @@ class MultiHeadAttention(keras.Model):
         self.proj = QDense(n_embd)
         self.dropout = keras.layers.Dropout(dropout)
 
-    def call(self, q, k):
-        out = keras.layers.concatenate([h(q, k) for h in self.heads], axis=-1)
+    def call(self, q, k, v, padding_mask=None, attn_mask=None):
+        out = keras.layers.concatenate([h(q, k, v, padding_mask, attn_mask) for h in self.heads], axis=-1)
         out = self.dropout(self.proj(out))
         
         # print("1: out.shape:", out.shape)

final_particle_transformer/particle_transformer.py
@@ -124,7 +124,7 @@ class ParticleTransformer(k.Model):
         if (v is not None or uu is not None) and self.pair_embed is not None:
             attn_mask = self.pair_embed(v, uu)
             attn_mask = tf.reshape(attn_mask, (-1, tf.shape(v)[-1], tf.shape(v)[-1]))
-        
+        print('padding_mask', padding_mask.shape, 'attn_mask', attn_mask.shape, 'x', x.shape)
         # transform
         for i, block in enumerate(self.blocks):
             print(f"Calling particle attention block...{i}", x.shape)